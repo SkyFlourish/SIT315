9178420456afba67fb97d4e716848d7f9e57fc7f
AJ NIKOLAOU <anikolao@deakin.edu.au>
2019-03-17 00:11:52 +1100

    Module 1  - First Upload
    Tasks 1 and 2 of Module 1

---
+++ Module 1/Task1-1P.ino
@@ -0,0 +1,32 @@
+//Setting up Constanants to describe pins
+const int sensor_output = 13;
+const int sensor_input = 2;
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+    // initialize the serial communications:
+    Serial.begin(9600);
+  //initialize digital pin sensor_output as an output
+  pinMode(sensor_output, OUTPUT);
+  //initialize digital pin sensor_input as an input
+  pinMode(sensor_input, INPUT);
+
+}
+
+void loop() {
+  val = digitalRead(sensor_input);
+  Serial.println(val);
+
+  if(val = HIGH)
+  {
+    digitalWrite(sensor_output, HIGH);
+  }
+  else
+  {
+    digitalWrite(sensor_output, LOW);
+  }
+  delay(1000);
+ 
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Module 1/Task1-2P.ino
@@ -0,0 +1,38 @@
+//Setting up Constanants to describe pins
+const int sensor_output = 13;
+const int interrupt_pin = 2;
+volatile byte state = LOW;
+
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+    // initialize the serial communications:
+    Serial.begin(9600);
+  //initialize digital pin sensor_output as an output
+  pinMode(sensor_output, OUTPUT);
+  
+  //initialize digital pin sensor_input as an input
+  pinMode(interrupt_pin, INPUT);
+  //initialize the intterupt ot work when the pin's value hanges from low to high
+  attachInterrupt(digitalPinToInterrupt(interrupt_pin),blink, CHANGE);
+
+
+}
+
+void loop() {
+// Serial.println(val);
+ // digitalWrite(sensor_output, state);
+
+ 
+  // put your main code here, to run repeatedly:
+  }
+
+  void blink () {
+    state = digitalRead(interrupt_pin);
+    //write on serial
+    Serial.println(interrupt_pin);
+    //write on pin output
+    digitalWrite(sensor_output,state);
+  }
+  
+
