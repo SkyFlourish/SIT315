f691606819a5d7c966f45e6fd3a792488ea85761
AJ NIKOLAOU <anikolao@deakin.edu.au>
2019-03-22 12:14:34 +1100

    Update on Task1-2P
    Fixed code

--- Module 1/Task1-2P.ino
+++ Module 1/Task1-2P.ino
@@ -10,29 +10,23 @@ void setup() {
     Serial.begin(9600);
   //initialize digital pin sensor_output as an output
   pinMode(sensor_output, OUTPUT);
-  
+
   //initialize digital pin sensor_input as an input
   pinMode(interrupt_pin, INPUT);
-  //initialize the intterupt ot work when the pin's value hanges from low to high
+  //initialize the intterupt to work when the pin's value hanges from low to high
   attachInterrupt(digitalPinToInterrupt(interrupt_pin),blink, CHANGE);
-
-
 }
 
 void loop() {
 // Serial.println(val);
  // digitalWrite(sensor_output, state);
-
- 
   // put your main code here, to run repeatedly:
   }
 
   void blink () {
     state = digitalRead(interrupt_pin);
     //write on serial
-    Serial.println(interrupt_pin);
+    Serial.println(state);
     //write on pin output
     digitalWrite(sensor_output,state);
   }
-  
-
